b01b43f24a997d0d728979c01bd1661fb6b9021b
egor4k <shapoval@biocad.ru>
2022-12-28 11:40:29 +0000

    update train dataset, add fictive function to train_sample_utils

--- train_sample_utils.py
+++ train_sample_utils.py
@@ -4,7 +4,7 @@ import random
 import pandas as pd
 from re import compile
 
-def get_ids_for_benchmark(path_to_structures: Path):
+def get_ids_from_directory(path_to_structures: Path):
     complex_dirs = os.listdir(path_to_structures)
     res = set()
     for comp_dir in complex_dirs:
@@ -16,6 +16,16 @@ def get_ids_for_benchmark(path_to_structures: Path):
         res.add(pdb_id)
     return res
 
+def get_ids_from_list(comp_list: list[str]):
+    res = set()
+    for comp_dir in comp_list:
+        i = 1
+        if "constr" in comp_dir:
+            i = 2
+        splitted = comp_dir.split("_")
+        pdb_id = splitted[i]
+        res.add(pdb_id)
+    return res
 
 def create_docked_equals_real(path_to_structures:Path,prefix:str='real',files_real=['real.pdb','joined_real.pdb'],files_docked=['docked.pdb','joined_docked.pdb'],file_lddt='lddt.csv'):
     path_to_structures=Path(path_to_structures)
@@ -36,7 +46,7 @@ def create_docked_equals_real(path_to_structures:Path,prefix:str='real',files_re
 def get_sample(path_to_structures: Path, test_set:list[str], size_ids = 100,size_constraits = 5,size_non_constraits = 5,size_real = 1,prefix_real = 'real',seed = 42):
     random.seed(seed)
     complex_dirs = os.listdir(path_to_structures)
-    pdb_ids=get_ids_for_benchmark(path_to_structures)
+    pdb_ids=get_ids_from_directory(path_to_structures)
     pdb_ids=set(pdb_ids)-set(test_set)
     sample_ids=random.sample(pdb_ids,size_ids)
     sample_list=[]
@@ -62,5 +72,8 @@ def list_from_file(path:str):
 
 if __name__=='__main__':
     test_set=list_from_file('benchmark_ids.txt')
-    a=get_sample(Path('/mnt/volume_complex_lddt/consistent_alpha_hedge'),test_set)
-    print(*a,sep='\n')
+    train_set=list_from_file('train_structures.txt')
+    train_set=get_ids_from_list(train_set)
+    #print(*train_set.union(test_set),sep='\n')
+    #a=get_sample(Path('/mnt/volume_complex_lddt/consistent_alpha_hedge'),train_set.union(test_set),size_ids=10)
+    #print(*a,sep='\n')
